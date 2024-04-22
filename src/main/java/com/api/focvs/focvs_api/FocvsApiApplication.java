02334dcdff891ba26fe58732ca5e988cbb78a1ef
FIRMINOenzo <enzofirmino.camp@gmail.com>
2024-04-22 15:55:26 -0300

    feat: initialize project

---
+++ src/main/java/com/api/focvs/focvs_api/FocvsApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.focvs.focvs_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocvsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocvsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/focvs/focvs_api/FocvsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.focvs.focvs_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocvsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
